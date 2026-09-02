d24a4abd74c5e2ee0be95b9211d3d0281b28f4e3
keremmkilincc-wq <keremmkilincc@gmail.com>
2026-09-02 18:15:13 +0300

    feat: joker 3x buyutuldu (0.018->0.055)

--- assets/app.js
+++ assets/app.js
@@ -247,8 +247,8 @@ mtlLoader.load('assets/models/joker.mtl', (mtl)=>{
   mtl.preload();
   objLoader.setMaterials(mtl);
   objLoader.load('assets/models/joker.obj', (obj)=>{
-    obj.scale.set(0.018,0.018,0.018);
-    obj.position.set(0,-0.15,0);
+    obj.scale.set(0.055,0.055,0.055);
+    obj.position.set(0,-0.55,0);
     obj.rotation.y=Math.PI;
     obj.traverse(c=>{ if(c.isMesh){ c.castShadow=true; c.receiveShadow=true; }});
     creature.remove(placeholder); creature.remove(phHead);
